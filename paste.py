f1ed7c95aef49ab2870be93b3ebc50e977475d43
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-07-31 18:33:18 +0530

    Update paste.py

--- paste.py
+++ paste.py
@@ -1,7 +1,9 @@
 from pythoncom import CoInitialize
+import subprocess
 from urllib.parse import unquote
 import win32clipboard
 from win32com import client
+import os
 
 global FLAG
 FLAG = 0
@@ -12,10 +14,19 @@ def run():
     data = win32clipboard.GetClipboardData()
     CoInitialize()
     shell = client.Dispatch("Shell.Application")
-    unquoted_path = list(shell.Windows())[-1].LocationURL[len("file:///"):]
-    most_recently_opened_window_path = unquote(unquoted_path)
-    path = most_recently_opened_window_path + "/mytxt.txt"
-    with open(path, "a") as f:
-        f.write(data)
-    FLAG = 1
+    if len(list(shell.Windows())) == 0:
+        with open( os.path.join(os.path.expanduser('~'), 'Desktop', 'mytxt.txt'), 'w') as f:
+            f.write(data)
+        FLAG = 1
+        return
+    else:
+        unquoted_path = list(shell.Windows())[-1].LocationURL[len("file:///"):]
+        most_recently_opened_window_path = unquote(unquoted_path)
+        if not os.path.isdir(most_recently_opened_window_path):
+            path = "/mytxt.txt"
+            subprocess.Popen(f'explorer /select, {most_recently_opened_window_path + "/mytxt.txt"}')
+        path = most_recently_opened_window_path + "/mytxt.txt"
+        with open(path, "a") as f:
+            f.write(data)
+        FLAG = 1
     return
